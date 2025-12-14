f48dc3000f7cf3df2fbda6d8f85047c4b6a06676
Vignesh D <vignesh.d9123@gmail.com>
2025-12-14 22:48:56 +0530

    Update model to 2.5 flash

--- app/api/summarize/route.ts
+++ app/api/summarize/route.ts
@@ -4,7 +4,7 @@ import { google } from "@ai-sdk/google"
 
 async function generateSummary(noteText: string): Promise<string> {
 
-  const model = google('gemini-2.0-flash')
+  const model = google('gemini-2.5-flash')
   const {text} = await generateText({
     model,
     system:'You are a note text summarizer. You will be given a note text and you will generate a summary of the textThe summary should be clear and concise. The summary should be formatted in markdown, with title, headings, and bullet points if needed.',
